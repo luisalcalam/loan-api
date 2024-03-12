599170ab1c61a49c7c28aae9a6c15cc5d0361edb
luisalcalam <luisalcala.ev@gmail.com>
2024-03-12 03:32:21 -0600

    Add entity and create user functionality

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,53 @@
-export class CreateUserDto {}
+import {
+  IsBoolean,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { UserRole } from '../../common/enums/userRoles';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  lastname;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @ApiProperty()
+  email;
+
+  @IsString()
+  @IsEnum(UserRole)
+  @IsOptional()
+  @ApiProperty()
+  role;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  @MaxLength(50)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'The password must have a Uppercase, lowercase letter and a number',
+  })
+  @ApiProperty()
+  password;
+
+  @IsBoolean()
+  @IsOptional()
+  @ApiProperty()
+  enabled?;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    const user = this.userRepository.create(createUserDto);
+
+    await this.userRepository.save(user);
+    return user;
   }
 
   findAll() {
