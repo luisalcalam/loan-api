bf26af97bb7d8acaf0d21ea1e6c8ee378b96033d
luisalcalam <luisalcala.ev@gmail.com>
2024-03-12 11:59:10 -0600

    Add protection in aplication services

--- src/loans/loans.controller.ts
+++ src/loans/loans.controller.ts
@@ -12,32 +12,39 @@ import { LoansService } from './loans.service';
 import { CreateLoanDto } from './dto/create-loan.dto';
 import { UpdateLoanDto } from './dto/update-loan.dto';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { UserRole } from 'src/common/enums/userRoles';
 
 @Controller('loans')
 export class LoansController {
   constructor(private readonly loansService: LoansService) {}
 
   @Post()
+  @Auth()
   create(@Body() createLoanDto: CreateLoanDto) {
     return this.loansService.create(createLoanDto);
   }
 
   @Get()
+  @Auth(UserRole.ADMIN)
   findAll(@Query() params: PaginationDto) {
     return this.loansService.findAll(params);
   }
 
   @Get(':id')
+  @Auth()
   findOne(@Param('id') id: string) {
     return this.loansService.findOne(id);
   }
 
   @Patch(':id')
+  @Auth()
   update(@Param('id') id: string, @Body() updateLoanDto: UpdateLoanDto) {
     return this.loansService.update(id, updateLoanDto);
   }
 
   @Delete(':id')
+  @Auth(UserRole.ADMIN)
   remove(@Param('id') id: string) {
     return this.loansService.remove(id);
   }
